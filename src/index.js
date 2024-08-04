c91e3cb0eba510ce5f83e5d417fd9dfb1fbf881a
DanielRivas18 <jose.rivas@uptc.edu.co>
2024-08-04 18:35:34 -0500

    new file:   package-lock.json 	modified:   package.json 	new file:   src/index.js 	new file:   src/swagger.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+//require('./drivers/connect-db')
+
+const app = express()
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+
+app.set('PORT', process.env.PORT || 3000)
+
+//app.use('/api/products', require('./routes/routes-products'))
+
+/* const swaggerUI = require('swagger-ui-express')
+const swaggerSpec = require('./swagger')
+app.use('/docs',swaggerUI.serve,swaggerUI.setup(swaggerSpec))
+ */
+app.listen(app.get('PORT'), () => console.log(`Server Ready al port ${app.get('PORT')}`))
